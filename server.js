9991787ae5e6e6038abebb5a0f428dba9513103d
TME <tyler.s.meyers@gmail.com>
2024-04-12 14:51:24 -0600

    trying to get html up and running

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-// const helpers = require(".utils/helpers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,15 +20,17 @@ const sess = {
   resave: false,
   store: new SequelizeStore({ db: sequelize }),
 };
+
 app.use(session(sess));
 
 //{ helpers }
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set('view engine", "handlebars');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
